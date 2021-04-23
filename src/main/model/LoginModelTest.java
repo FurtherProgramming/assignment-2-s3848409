717c035822b09238817f2f0f2ed1be89bd64f446
Peter2707 <s3848409@student.rmit.edu.au>
2021-04-23 20:12:46 +1000

    done project config

---
+++ src/main/model/LoginModelTest.java
@@ -0,0 +1,14 @@
+package main.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginModelTest {
+
+    @org.junit.jupiter.api.Test
+    void isDbConnected() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isLogin() {
+    }
+}
